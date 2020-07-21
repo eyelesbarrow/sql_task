b90e3e20fa2791478d681aae295b420eb7a52cc1
Kay Alave <k.alave@gmail.com>
2020-07-21 21:05:00 +0200

    Update sql_report.py

--- sql_report.py
+++ sql_report.py
@@ -4,12 +4,13 @@ from sqlite3 import Error
 
 
 
-statement = """select id_account as id_person, strftime('%m.%Y', transaction_date) as month, 
-ifnull(count(1),0) as sum_of_transactions
-from transactions
-where id_account in (345, 1234)
-and transaction_date between '2020-02-15' and '2020-06-06'
-group by id_account, month"""
+statement = """SELECT id_account AS id_person, 
+strftime('%m.%Y', transaction_date) AS month, 
+ifnull(count(1),0) AS sum_of_transactions
+FROM transactions
+WHERE id_account in (345, 1234)
+AND transaction_date between '2020-02-15' AND '2020-06-06'
+GROUP BY id_account, month"""
 
 
 def sql_query(statement): 
